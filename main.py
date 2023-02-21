b8cec8a1ce9126bc27fbca8e3b1f60283c3fb20c
StarbuckBarista <carlitos.salazar@protonmail.com>
2023-02-21 14:41:43 -0600

    Fixed Win Checking

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ class Board:
         # Check Lines
         for index in range(2):
             row = self.position[index]
-            column = [row[index] for row in this.position]
+            column = [row[index] for row in self.position]
 
             if row[0] == row[1] == row[2]: return row[0]
             if column[0] == column[1] == column[2]: return column[0]
 
         # Check Diagonals
-        diagonal_one = [this.position[index][index] for index in range(2)]
-        diagonal_two = [this.position[index][2 - index] for index in range(2)]
+        diagonal_one = [self.position[index][index] for index in range(3)]
+        diagonal_two = [self.position[index][2 - index] for index in range(3)]
 
         if diagonal_one[0] == diagonal_one[1] == diagonal_one[2]: return diagonal_one[0]
         if diagonal_two[0] == diagonal_two[1] == diagonal_two[2]: return diagonal_two[0]
